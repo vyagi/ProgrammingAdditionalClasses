87ed926305f76c4c6d357666fad34488e0a1e9eb
Małgorzata Jagieła <malgorzata.jagiela@hotmail.com>
2025-03-13 16:54:17 +0100

    Generics introduced

---
+++ ProgrammingInCsharp/MoreCSharpFeatures/Program.cs
@@ -0,0 +1,33 @@
+﻿var p1 = new Pair<int>(1, 2);
+var p2 = new Pair<string>("a", "b");
+var p3 = new Pair<DateTime>(DateTime.Now, DateTime.Today);
+
+var listPhones = new List<Phone>();
+var l1 = new List<string>();
+var l2 = new List<int>();
+
+l1.Add("James Bond");
+l2.Add(5);
+
+foreach (var x in l1)
+{
+    Console.WriteLine(x.Length);
+}
+
+foreach (var x in l2)
+{
+    Console.WriteLine(x + 10);
+}
+
+public class Pair<T>
+{
+    public T First;
+    public T Second;
+
+    public Pair(T first, T second)
+    {
+        First = first;
+        Second = second;
+    }
+}
+public class Phone{}
