290871fcd7fe27514bb2f535db21fe757a4440cc
Małgorzata Jagieła <malgorzata.jagiela@hotmail.com>
2025-03-13 16:24:26 +0100

    Polymorphism done

--- ProgrammingInCsharp/MoreObjectOrientedProgramming/Program.cs
+++ ProgrammingInCsharp/MoreObjectOrientedProgramming/Program.cs
@@ -1,17 +1,49 @@
 ﻿//Encapsulation [v]
 //Inheritance [v]
-//Polymorphism [ ]
+//Polymorphism [v] - it means that the method will not be chosen depending on the variable type
+// but depending on the actual object type. To enable polymorphism you need virtual/override keywords.
 //Abstraction [ ]
 
-Phone phone = new Phone("123456789");
-phone.Call("987654321");
+#region Ihneritance demo
+//Phone phone = new Phone("123456789");
+//phone.Call("987654321");
 
-WirelessPhone wirelessPhone = new WirelessPhone("111222333");
-wirelessPhone.Charge();
-wirelessPhone.Call("11111111111");
-wirelessPhone.ShowOwnNumber();
+//WirelessPhone wirelessPhone = new WirelessPhone("111222333");
+//wirelessPhone.Charge();
+//wirelessPhone.Call("11111111111");
+//wirelessPhone.ShowOwnNumber();
+#endregion
 
+#region Polymorphism demo
 
+#endregion
+
+Phone p1 = new Phone("123456789");
+WirelessPhone p2 = new WirelessPhone("987654321");
+WirelessPhone p3 = new WirelessPhone("111222333");
+p3.Charge();
+
+PhoneCollection phoneCollection = new PhoneCollection();
+phoneCollection.AddPhone(p1);
+phoneCollection.AddPhone(p2);
+phoneCollection.AddPhone(p3);
+
+phoneCollection.CallAll("999999999");
+
+public class PhoneCollection
+{
+    private List<Phone> _phones = new List<Phone>();
+
+    public void AddPhone(Phone phone) => _phones.Add(phone);
+
+    public void CallAll(string otherNumber)
+    {
+        foreach (Phone phone in _phones)
+        {
+            phone.Call(otherNumber);
+        }
+    }
+}
 
 public class WirelessPhone : Phone
 {
@@ -19,7 +51,7 @@ public class WirelessPhone : Phone
 
     public WirelessPhone(string ownNumber) : base(ownNumber) { }
 
-    public void Call(string otherNumber)
+    public override void Call(string otherNumber)
     {
         if (_batteryLevel > 0)
         {
@@ -48,7 +80,7 @@ public class Phone
 
     public Phone(string ownNumber) => _ownNumber = ownNumber;
 
-    public void Call(string otherNumber)
+    public virtual void Call(string otherNumber)
     {
         if (_ownNumber == otherNumber)
             Console.WriteLine("Biip biip biip");
